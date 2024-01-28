e38195255d786d2a260d30bb433ef0cd417c524f
1963street <138835743+1963street@users.noreply.github.com>
2024-01-28 10:24:52 -0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
+        <a href="https://clarkstreetsalon.com" target="_blank">
           start.solidjs.com
         </a>{" "}
         to learn how to build SolidStart apps.
